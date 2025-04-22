d9ba9de85eae8d180923bbeb1c262edfa417054f
Anastasiia Yerashova <yerashova.a@gmail.com>
2025-04-22 15:57:14 +0200

    setup firebase config and add sensitive data to .env

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
+
+const {VITE_API_KEY, VITE_AUTH_DOMAIN, VITE_DB_DOMAIN, VITE_PROJECT_ID, VITE_STORAGE_BUCKET, VITE_MESSAGING_SENDER_ID, VITE_APP_ID} = import.meta.env
+
+const firebaseConfig = {
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  databaseURL: VITE_DB_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGING_SENDER_ID,
+  appId: VITE_APP_ID
+};
+
+export const app = initializeApp(firebaseConfig)
+export const db = getDatabase()
+export const auth = getAuth(app)
