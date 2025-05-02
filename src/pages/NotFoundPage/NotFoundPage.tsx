8b151ebfa17fffff55ffb8e43a61de46fde14161
Anastasiia Yerashova <yerashova.a@gmail.com>
2025-05-03 01:11:36 +0200

    add NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom"
+import s from './NotFoundPage.module.css'
 
 
 const NotFoundPage = () => {
     return (
-        <p>ntf</p>
+            <section className={s.ntf_section}>
+                <div className={s.content}>
+                    <h1 className={s.title}>Oops! We couldn't find the page</h1>
+                    <h2 className={s.subtitle}>But don't worry, you can always go back to the homepage</h2>
+                    <Link to='/' className={s.btn}>Go to home</Link>
+                </div>
+            </section>
     )
 }
 
